106b72bccbea5e4797df91dc11f32e02e76cd489
Stiven Ramírez Arango <sramirez@anutibara.com>
2019-12-31 09:25:57 -0500

    Adding Form Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation title="My first navigation" />
-      <Navigation title="My second navigation" />
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+import { todos } from './todos.json';
+console.log(todos);
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
+  render(){
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+          <div className="card-header">
+            <h3>{ todo.title }</h3>
+            <span className="badge badge-pill badge-danger ml-2">
+              { todo.priority }
+            </span>
+          </div>
+          <div className="card-body">
+            <p>{ todo.description }</p>
+            <p><mark>{ todo.responsible }</mark></p>
+          </div>
+        </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        <Navigation title="Tasks" counter={ this.state.todos.length } />
+        <div className="container">
+          <div className="row mt-4">
+            { todos }
+          </div>  
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,13 @@ class Navigation extends Component {
             <nav className="navbar navbar-dark bg-dark">
                 <a href="/" className="text-white">
                 { this.props.title }
+                <span className="badge badge-pill badge-light ml-2">
+                { this.props.counter }
+                </span>
                 </a>
             </nav>
         )
     }
 }
 
-export default Navigation;
+export default Navigation; 
